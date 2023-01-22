c524dcaca5fc5cc77704d9ac73eaac85418b7714
lucianogomes02 <luciano@pontotel.com.br>
2023-01-22 03:47:52 -0300

    feat(src/views/Login.vue): inicializando a view de Login

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: "Login",
+    components: {},
+    data (){},
+  
+  });
+  
+  </script>
+  
+  
+  <style scoped>
+  
+  </style>
+  

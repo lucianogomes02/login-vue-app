23724d5d9321fc9c751292e9f1b05ad4c3af2ff8
lucianogomes02 <luciano@pontotel.com.br>
2023-01-24 01:48:25 -0300

    lint

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -45,10 +45,10 @@ export default defineComponent({
 </script>
 
 <style scoped>
-@import url('https://fonts.cdnfonts.com/css/assistant');
+@import url("https://fonts.cdnfonts.com/css/assistant");
 
 * {
-  font-family: 'Assistant', sans-serif;
+  font-family: "Assistant", sans-serif;
   font-weight: lighter;
 }
 

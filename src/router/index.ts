bbeb6fc4ee20d022c3ed2183df06652ef9b8ff0c
lucianogomes02 <luciano@pontotel.com.br>
2023-01-27 00:42:41 -0300

    feat(src/views/CadastroView.vuw): implementa componente CadastroView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/LoginView.vue";
+import Cadastro from "../views/CadastroView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +11,9 @@ const router = createRouter({
       component: Login,
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/LoginView.vue"),
+      path: "/cadastro",
+      name: "cadastro",
+      component: Cadastro,
     },
   ],
 });

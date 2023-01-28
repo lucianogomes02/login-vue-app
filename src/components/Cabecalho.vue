95161605eab01bc9d27d8847a30dd430341eea08
lucianogomes02 <luciano@pontotel.com.br>
2023-01-27 23:36:26 -0300

    style(src/views/CadastroView.vue): inicializa estilização e destribuição dos elemento da view de Cadastro

---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="cabecalho">
+
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "Cabecalho"
+});
+</script>
+
+<style scoped>
+.cabecalho {
+    background: #00a3ff;
+    width: 100%;
+    height: 6vh;
+    margin: -8px;
+}
+
+</style>
--- src/views/CadastroView.vue
+++ src/views/CadastroView.vue
@@ -1,13 +1,35 @@
 <template>
+    <main class="containerCadastro">
+        <Cabecalho></Cabecalho>
+        <body class="cadastro"></body>
+    </main>
     
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Cabecalho from "../components/Cabecalho.vue";
 
 export default defineComponent({
   name: "CadastroView",
+  components: { Cabecalho },
 });
 </script>
 
-<style></style>
+<style scoped>
+.containerCadastro {
+  display: flex;
+  margin: -8px;
+  
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+
+.cadastro {
+    background: #8fddf5;
+    width: 100%;
+    height: 95.7vh;
+    margin: -1px;
+}
+</style>

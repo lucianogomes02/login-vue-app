4abc111b55028c9f42e5b5d602468f7f935ea3d2
lucianogomes02 <luciano@pontotel.com.br>
2023-03-02 18:20:35 -0300

    feat(src/components/UsuarioView.vue): adiciona seção de informações do usuário logado à UsuarioView

--- src/views/UsuarioView.vue
+++ src/views/UsuarioView.vue
@@ -1,17 +1,20 @@
 <template>
-    <main class="container__usuario">
-        <Cabecalho></Cabecalho>
-    </main>
+  <main class="container__usuario">
+    <Cabecalho></Cabecalho>
+    <section class="usuario">
+      <div class="informacoes__do__usuario"></div>
+    </section>
+  </main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 import Cabecalho from "../components/Cabecalho.vue";
 
 export default defineComponent({
-    name: "UsuarioView",
-    components: { Cabecalho },
-})
+  name: "UsuarioView",
+  components: { Cabecalho },
+});
 </script>
 
 <style scoped>
@@ -22,4 +25,17 @@ export default defineComponent({
   margin: -8px;
 }
 
+.usuario {
+  padding: 10rem;
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: center;
+}
+
+.informacoes__do__usuario {
+  background-color: #b3e7f77c;
+  width: 80rem;
+  height: 40rem;
+  border-radius: 2rem;
+}
 </style>
